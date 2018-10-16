e575118e35e1f491217c512b9552b1eba33a68a5
felixjtdb <felixjtdb@gmail.com>
2018-10-16 17:08:09 +0100

    players are able to be input

--- app.rb
+++ app.rb
@@ -4,4 +4,12 @@ class Battle < Sinatra::Base
   get "/" do
     "Testing infrastructure working!"
   end
+  get "/names" do
+    erb(:names_form)
+  end
+  post "/play" do
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb(:play)
+  end
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -3,4 +3,11 @@ feature 'Testing infrastructure' do
     visit '/'
     expect(page).to have_content 'Testing infrastructure working!'
   end
+  scenario 'two player names can be entered' do
+    visit '/names'
+    fill_in('player1', with: 'John')
+    fill_in('player2', with: 'Tim')
+    click_button 'Submit'
+    expect(page).to have_content "John Tim"
+  end
 end
